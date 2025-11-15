1ff5e2d56d44885508d07c010e4ec05c19d90f54
x6bi <adamking8637@gmail.com>
2025-11-15 23:40:45 +0100

    feat: add system information display in renderer process

--- electron/main.js
+++ electron/main.js
@@ -1,7 +1,12 @@
 const {app, BrowserWindow} = require('electron');
+const path = require('path');
 
 const createWindow = () => {
-  const win = new BrowserWindow({width: 800, height: 600});
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {preload: path.join(__dirname, 'preload.js')}
+  });
   win.loadFile('./renderer/index.html');
 };
 
---
+++ electron/renderer/script.js
@@ -0,0 +1,4 @@
+const info = document.getElementById('info');
+info.innerText =
+    `This app is using Chrome (v${window.versions.chrome()}), Node.js (v${
+        window.versions.node()}), and Electron (v${window.versions.electron()})`
